ef9749099b9e7f9fb63d3fdb70bef75e43956155
Kevin Byrne <kevinbyrne@lawn-143-215-52-206.lawn.gatech.edu>
2020-08-08 18:53:19 -0400

    Hooked up firebase

--- index.js
+++ index.js
@@ -1,28 +1,78 @@
 require('dotenv').config()
 const express = require('express')
-const request = require('request')
+const admin = require('firebase-admin');
 const path = require('path')
 const app = express()
+const bodyParser = require('body-parser');
 const port = 3000
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public/views'))
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const serviceAccount = require('./house-check-in-firebase-adminsdk-47eql-4a82ecca03.json');
+const { exit } = require('process');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://house-check-in.firebaseio.com"
+  });
+
+const db = admin.firestore();
 
 app.get('/', (req, res) => {
     const data = {
-        commonGuests: 10,
-        totalGuests: 20
+        brothers: []
     }
-    res.render('pages/index', data);
+    db.collection('brothers').get().then(snapshot => {
+        snapshot.forEach(documentSnapshot => {
+            data.brothers.push(documentSnapshot.data())
+        })
+        data.brothers.sort((a, b) => { return a.last.localeCompare(b.last) })
+        res.render('pages/index', data)
+    })
 });
 
-app.get('/check-in', (req, res) => {
-    res.render('pages/check-in');
-});
+app.post('/check-in', (req, res) => {
+    const b_last = req.body.brother.split(', ')[0]
+    const b_first = req.body.brother.split(', ')[1]
+    db.collection('check-ins').add({
+        b_last: b_last,
+        b_first: b_first,
+        g_last: null,
+        g_first: null,
+        timestamp: new admin.firestore.Timestamp(Math.floor(new Date().getTime() / 1000), 0)
+    })
+    if (req.body.guest1_first) {
+        db.collection('check-ins').add({
+            b_last: b_last,
+            b_first: b_first,
+            g_last: req.body.guest1_last,
+            g_first: req.body.guest1_first,
+            timestamp: new admin.firestore.Timestamp(Math.floor(new Date().getTime() / 1000), 0)
+        })
+    }
+    if (req.body.guest2_first) {
+        db.collection('check-ins').add({
+            b_last: b_last,
+            b_first: b_first,
+            g_last: req.body.guest2_last,
+            g_first: req.body.guest2_first,
+            timestamp: new admin.firestore.Timestamp(Math.floor(new Date().getTime() / 1000), 0)
+        })
+    }
+    res.render('pages/success')
+})
 
-app.get('/check-out', (req, res) => {
-    res.render('pages/check-out');
-});
+app.get('/admin', (req, res) => {
+    res.render('pages/admin')
+})
 
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
+app.get('/admin-restart', (req, res) => {
+    exit()
+})
+
+app.listen(port, () => {
+    console.log('Listening on port 3000')
+})
---
+++ populate.js
@@ -0,0 +1,20 @@
+const admin = require('firebase-admin');
+const csv = require('csv-parser');
+const fs = require('fs');
+const serviceAccount = require('./house-check-in-firebase-adminsdk-47eql-4a82ecca03.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://house-check-in.firebaseio.com"
+});
+const db = admin.firestore();
+const batch = db.batch()
+
+fs.createReadStream('brothers.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    const ref = db.collection('brothers').doc();
+    batch.set(ref, row);
+  })
+  .on('end', () => {
+    batch.commit()
+  });
